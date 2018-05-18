7375f312e690cd53ee65e7424ef2457bd6f0897b
vasavipragya <vasavipragya@gmail.com>
2018-05-18 16:26:25 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var message = document.getElementById("message");
+var doneBtn=document.getElementById("doneBtn");
+
+	function doneClick() {
+		var name = document.getElementById("name").value;
+		var email = document.getElementById("email").value;
+		var message = document.getElementById("message").value;
+
+
+		var firebaseRef = firebase.database().ref();
+
+		firebaseRef.child("text").set("some value");
+
+	}
