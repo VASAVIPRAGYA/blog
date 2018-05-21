95d5e4b192cbda8e0d8cbaabb1ac250ff026ad29
vasavipragya <vasavipragya@gmail.com>
2018-05-21 16:46:55 +0530

    remove post1.js , remove pos

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var message = document.getElementById("message");
-var doneBtn=document.getElementById("doneBtn");
+function doneClick(){
 
-	function doneClick() {
 		var name = document.getElementById("name").value;
 		var email = document.getElementById("email").value;
 		var message = document.getElementById("message").value;
 
+		var postData = {
+			name:name,
+			email:email,
+			message:message
+		};
 
-		var firebaseRef = firebase.database().ref();
 
-		firebaseRef.child("text").set("some value");
+		var database = firebase.database();
 
-	}
+		var newPostKey = database.ref().child('user').push().key;
+
+		var updates = {};
+		updates['/users/' + newPostKey] = postData;
+		//updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+		return firebase.database().ref().update(updates);
+
+
+}
